d41824aa669d03a0d5e845ab6f23d62a11b1324c
Yongki Jay An <hinza0188@users.noreply.github.com>
2017-04-10 00:18:26 -0400

    Login (#1)
    * created new string field on user column, and added seeder form
    
    * before apply, $ php artisan migrate:fresh  command required
    
    * following laravel docs
    
    * revert back to where seeder worked fine
    
    * added authentication php files
    
    * added new seeder for admin login
    
    * added admin@rit.edu in the seeder, and deprecated welcome page
    
    * added new base layout blade and changed all other blade's extention pointer to it
    
    * login completed
    
    * login / register feature completed
    
    * fixed register bug
    
    * fixed two other forms, but figured laravel needs to be set with email servers. We simply don't have one :(
    
    * re-migrated the db from merge conflict
    
    * now ignoring the database
    
    * added empty db file for initial migration requirement

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,27 @@
-<!DOCTYPE html>
 
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <title>Home</title>
-
-</head>
-
-<body>
-    <h1> dashboard </h1>
-    <a href="browse"> browse events</a>
-    <a href="create"> create event </a>
-    <a href="account"> account </a>
-</body>
-
-</html>
-
-<?php
+<!-- <Jay>: Not within my scope -->
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            @if(Auth::check())
+            <div class="card card-primary">
+                <br>Great, you are logged in :)
+            </div>
+            @else
+            <div class="card card-danger">
+                <br>Bad :( you shouldn't be able to see this
+            </div>
+            @endif
+        </div>
+        <div class="col-md-8 col-md-offset-2">
+            <h1> dashboard </h1>
+            <a href="browse"> browse events</a>
+            <a href="create"> create event </a>
+            <a href="account"> account </a>
+        </div>
+    </div>
+</div>
+@endsection
