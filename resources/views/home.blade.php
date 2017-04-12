1a152c3ac4c0f5dfc17df314f6488e1bbf22dc85
Yongki Jay An <hinza0188@users.noreply.github.com>
2017-04-12 17:16:18 -0400

    Base fix (#10)
    * moved out basepage's css into seperate css file in public directory
    
    * now the base page is linked to the basepage
    
    * added comments where the css file exist
    
    * moved script file out into separte js file
    
    * syntax error :( fixed
    
    * saving progress
    
    * made new all_base file for greater inheritance
    
    * okay this seems fine
    
    * now all pages that directly extends from all_base now has body tag
    
    * changed some comments

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,19 @@
-@extends('layouts.basePage')
+@extends('layouts.base_page')
+
+@section('css')
+    <link rel="stylesheet" href="{{asset('css/dashboard.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('css/parallax/style.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('css/slideshow/slideshow.css')}}" type="text/css">
+@endsection
+
+@section('js')
+    <script src="{{asset('js/slideshow.js')}}"></script>
+    <script src="{{asset('js/scroll.js')}}"></script>
+    <script>
+        // load parallax javascript
+        loadParallaxEffect();
+    </script>
+@endsection
 
 @section('content')
 
@@ -216,18 +231,5 @@
 
 </main>
 
-
-
-<script src="{{asset('js/jquery-3.2.0.min.js')}}"></script>
-<link rel="stylesheet" href="{{asset('css/dashboard.css')}}" type="text/css">
-<link rel="stylesheet" href="{{asset('css/parallax/style.css')}}" type="text/css">
-<link rel="stylesheet" href="{{asset('css/slideshow/slideshow.css')}}" type="text/css">
-<script src="{{asset('js/slideshow.js')}}"></script>
-<script src="{{asset('js/scroll.js')}}"></script>
-
-<script>
-    // load parallax javascript
-    loadParallaxEffect();
-</script>
 
 @endsection
---
+++ resources/views/layouts/all_base.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        {{--<link href="/css/app.css" rel="stylesheet"> <!-- Using this causes error after login --> --}}
+        @yield('base_css')
+
+    </head>
+    @yield('body')
+    <!-- Scripts -->
+    <script>
+        window.Laravel = {!! json_encode([
+                'csrfToken' => csrf_token(),
+            ]) !!};
+    </script>
+    @yield('base_js')
+</html>
