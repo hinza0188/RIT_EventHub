aecda55d0e74a3317e39828fcc3fda8856695ca4
Yongki Jay An <hinza0188@users.noreply.github.com>
2017-04-14 19:47:49 -0400

    Account3 (#19)
    * changed redirection methods in laravel syntax
    
    * fixed error
    
    * now event route is in resource format, and base page now looks upon laravel syntaxed route

--- resources/views/account/dob/edit.blade.php
+++ resources/views/account/dob/edit.blade.php
@@ -2,11 +2,36 @@
 
 @section('content')
     <div class="container" style="margin-top: 168.88px;">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                this is dob edit page
-                {{$dob}}
+        <h2>Edit Date of Birth</h2>
+
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
             </div>
+        @endif
+
+        {!! Form::model($dob, ['method' => 'PATCH','route' => ['account_dob.update', $dob->id]]) !!}
+        <div class="row">
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Date: </strong>
+                    {!! Form::date('date') !!}
+                </div>
+            </div>
+
+            <input style="display:none" name="user_id" value="{{Auth::id()}}">
+
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+
         </div>
+        {!! Form::close() !!}
     </div>
 @endsection
