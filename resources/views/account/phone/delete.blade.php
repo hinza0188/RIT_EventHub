6e167fb5260034c9f1a21cffa7037d2c518dcf72
Yongki Jay An <hinza0188@gmail.com>
2017-04-14 17:55:13 -0400

    nope never mind...

--- resources/views/account/phone/delete.blade.php
+++ resources/views/account/phone/delete.blade.php
@@ -1,14 +1,5 @@
 @extends('layouts.base_page')
 
-@section('css')
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-@endsection
-
-@section('js')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-@endsection
-
 @section('content')
     <div class="container" style="margin-top: 168.88px;">
         <div class="row">
