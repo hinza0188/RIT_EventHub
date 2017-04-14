5b991ef635881e7d2c3b4be91b96e3ed4c0b3f8c
Yongki Jay An <hinza0188@gmail.com>
2017-04-14 19:16:25 -0400

    changed redirection methods in laravel syntax

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -14,9 +14,12 @@
         <div>
             Date of birth:
             @if(sizeof($dob)>0)
-                {{$dob[0]->date}} <a href="/account_dob/{{$dob[0]->id}}/edit"><i class="fa fa-fw fa-pencil-square-o" aria-hidden="true"></i></a>
+                {{$dob[0]->date}} <a href="{{ route( 'account_dob.edit', ['$id' => $dob[0]->id] ) }}"><i class="fa fa-fw fa-pencil-square-o" aria-hidden="true"></i></a>
             @else
-                <a href="/account_dob/create"><i class="fa fa-fw fa-plus-circle" aria-hidden="true"></i></a>
+                {!! Form::open([ 'method'  => 'delete', 'route' => [ 'account_dob.destroy', $dob[0]->id ] ]) !!}
+                    <a href="{{ route('account_dob.create') }}"><i class="fa fa-fw fa-plus-circle" aria-hidden="true"></i></a>
+                    {{ Form::submit('Delete', ['class' => 'btn btn-danger btn-sm'] ) }}
+                {{ Form::close() }}
             @endif
         </div>
         @if(sizeof($phones) > 0)
@@ -26,20 +29,20 @@
                     Phone Number [ {{$phone->name}} ]: {{$phone->number}}
                     <a href="{{ route( 'account_phone.create' ) }}"><i class="fa fa-plus-circle fa-fw" aria-hidden="true"></i></a>
                     <a href="{{ route( 'account_phone.edit', ['$id' =>$phone->id] ) }}"><i class="fa fa-fw fa-pencil-square-o" aria-hidden="true"></i></a>
-                    {{ Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) }}
+                    {{ Form::submit('Delete', ['class' => 'btn btn-danger btn-sm'] ) }}
                 </div>
             {{ Form::close() }}
             @endforeach
         @else
             <div>
-                Phone Number: <a href="account_phone/create"><i class="fa fa-plus-circle fa-fw" aria-hidden="true"></i></a>
+                Phone Number: <a href="{{ route( 'account_phone.create') }}"><i class="fa fa-plus-circle fa-fw" aria-hidden="true"></i></a>
             </div>
         @endif
     </div>
     <div class="container">
         <div class="btn-group">
             <a href="/account">Reset Password</a>
-            <a href="/account/{{Auth::user()->id}}/edit">Edit Account</a>
+            <a href="{{ route( 'account.edit', ['$id' => Auth::id()] ) }}">Edit Account</a>
         </div>
     </div>
 @endsection
