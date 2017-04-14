0746ec47b69ffd12b74cdf60731efd6439f855ff
Yongki Jay An <hinza0188@gmail.com>
2017-04-14 17:42:28 -0400

    added little more bootstrap on account index page

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -24,26 +24,24 @@
                         Last Name: {{Auth::user()->last_name}}
                     </div>
                     <div>
-                        Date of birth: @if($dob){{$dob[0]->date}} <a class="btn" href="/account_dob/{{$dob[0]->id}}/edit">Edit</a>@else <a href="/account_dob/create" class="btn">Register Date of Birth</a> @endif
+                        Date of birth: @if($dob){{$dob[0]->date}} <a class="btn btn-success btn-md" href="/account_dob/{{$dob[0]->id}}/edit">Edit</a>@else <a href="/account_dob/create" class="btn btn-primary btn-md">Register Date of Birth</a> @endif
                     </div>
                     @if(sizeof($phones) > 0)
                         @foreach($phones as $phone)
                         <div>
-                            Phone Number [ {{$phone->name}} ]: {{$phone->number}} <a class="btn" href="/account_phone/{{$phone->id}}/edit">Edit</a>
+                            Phone Number [ {{$phone->name}} ]: {{$phone->number}} <a class="btn btn-success btn-md" href="/account_phone/{{$phone->id}}/edit">Edit</a>
                         </div>
                         @endforeach
                     @else
                         <div>
-                            Phone Number: <a href="/account_phone/create" class="btn">Register new phone number</a>
+                            Phone Number: <a href="/account_phone/create" class="btn btn-primary btn-md">Register phone number</a>
                         </div>
                     @endif
                 </div>
                 <div class="container">
-                    <div>
-                        <a href="/account">Reset Password</a>
-                    </div>
-                    <div>
-                        <a href="/account/{{Auth::user()->id}}/edit">Edit Account</a>
+                    <div class="btn-group">
+                        <a class="btn btn-warning btn-md" href="/account">Reset Password</a>
+                        <a class="btn btn-success btn-md" href="/account/{{Auth::user()->id}}/edit">Edit Account</a>
                     </div>
                 </div>
             </div>
