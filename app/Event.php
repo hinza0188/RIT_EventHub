ea5f7958baa79ae228c68e92476df763d99a0faf
Yongki Jay An <hinza0188@gmail.com>
2017-05-09 13:29:46 -0400

    commneted out event model's overriding creator id

--- app/Event.php
+++ app/Event.php
@@ -13,15 +13,15 @@ class Event extends Model
         'title', 'description', 'location', 'imgURL', 'max', 'joined', 'interested', 'date',
     ];
 
-    public function save(array $options = array())
-    {
-        if( ! $this->creator_id)
-        {
-            $this->creator_id = Auth::user()->id;
-        }
-
-        parent::save($options);
-    }
+//    public function save(array $options = array())
+//    {
+//        if( ! $this->creator_id)
+//        {
+//            $this->creator_id = Auth::user()->id;
+//        }
+//
+//        parent::save($options);
+//    }
 
     public function get_users() {
         return $this->belongsToMany(User::class, 'event_user', 'event_id', 'user_id');
